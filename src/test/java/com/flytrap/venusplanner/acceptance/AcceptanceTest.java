3861e989a4a5741246863c2d0686da21713dc970
Ju, Seonghwan (Ape) <crtevn@gmail.com>
2024-03-29 14:16:59 +0900

    refactor: AcceptanceTest에 응답코드 검증 세분화

--- src/test/java/com/flytrap/venusplanner/acceptance/AcceptanceTest.java
+++ src/test/java/com/flytrap/venusplanner/acceptance/AcceptanceTest.java
@@ -6,7 +6,8 @@ import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.server.LocalServerPort;
@@ -15,18 +16,17 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @Sql("classpath:reset.sql")
 public abstract class AcceptanceTest {
 
     @LocalServerPort
     private int port;
 
-    @BeforeEach
+    @BeforeAll
     void setUp() {
         RestAssured.port = port;
     }
-
-    // TODO:로그인 로직
 
     protected static RequestSpecification givenJsonRequest() {
         return RestAssured.given().log().all()
@@ -34,7 +34,27 @@ public abstract class AcceptanceTest {
                 .contentType(MediaType.APPLICATION_JSON_VALUE);
     }
 
-    protected void 응답_상태코드_검증(ExtractableResponse<Response> response, HttpStatus httpStatus) {
+    protected void 응답_코드는_200_OK_를_반환한다(ExtractableResponse<Response> response) {
+        응답_상태코드_검증(response, HttpStatus.OK);
+    }
+
+    protected void 응답_코드는_201_CREATED_를_반환한다(ExtractableResponse<Response> response) {
+        응답_상태코드_검증(response, HttpStatus.CREATED);
+    }
+
+    protected void 응답_코드는_403_FORBIDDEN_를_반환한다(ExtractableResponse<Response> response) {
+        응답_상태코드_검증(response, HttpStatus.FORBIDDEN);
+    }
+
+    protected void 응답_코드는_404_NOT_FOUND_를_반환한다(ExtractableResponse<Response> response) {
+        응답_상태코드_검증(response, HttpStatus.NOT_FOUND);
+    }
+
+    protected void 응답_코드는_409_CONFLICT_를_반환한다(ExtractableResponse<Response> response) {
+        응답_상태코드_검증(response, HttpStatus.CONFLICT);
+    }
+
+    private void 응답_상태코드_검증(ExtractableResponse<Response> response, HttpStatus httpStatus) {
         assertThat(response.statusCode()).isEqualTo(httpStatus.value());
     }
 }
