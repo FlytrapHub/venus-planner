9eb4554f7366ed7cbf9c2e206dc7d0cdefe38ccf
Ju, Seonghwan (Ape) <crtevn@gmail.com>
2024-03-29 14:22:04 +0900

    test: JoinRequestStep 추가
    - 스터디 가입 요청
    - 스터디 가입 요청 수락
    - 스터디 가입 요청 거절

---
+++ src/test/java/com/flytrap/venusplanner/acceptance/join_request/step/JoinRequestStep.java
@@ -0,0 +1,31 @@
+package com.flytrap.venusplanner.acceptance.join_request.step;
+
+import com.flytrap.venusplanner.acceptance.AcceptanceTest;
+import com.flytrap.venusplanner.acceptance.common.SessionCookie;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+
+public class JoinRequestStep extends AcceptanceTest {
+
+    public static ExtractableResponse<Response> 스터디_가입_요청(Long studyId, SessionCookie sessionCookie) {
+        return givenJsonRequest()
+                .cookie(sessionCookie.name(), sessionCookie.value())
+                .when().post("/api/v1/studies/{studyId}/join-requests", studyId)
+                .then().log().all().extract();
+    }
+
+    public static ExtractableResponse<Response> 스터디_가입_요청_수락(Long studyId, Long requestId, SessionCookie sessionCookie) {
+        return givenJsonRequest()
+                .cookie(sessionCookie.name(), sessionCookie.value())
+                .when().post("/api/v1/studies/{studyId}/join-requests/{requestId}/accept", studyId, requestId)
+                .then().log().all().extract();
+    }
+
+    public static ExtractableResponse<Response> 스터디_가입_요청_거절(Long studyId, Long requestId, SessionCookie sessionCookie) {
+        return givenJsonRequest()
+                .cookie(sessionCookie.name(), sessionCookie.value())
+                .when().post("/api/v1/studies/{studyId}/join-requests/{requestId}/reject", studyId, requestId)
+                .then().log().all().extract();
+    }
+
+}
